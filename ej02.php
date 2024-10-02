fbb7b59d7277d0e24c312e4e7d2cb6a5477721ba
gcarpen480 <158757674+gcarpen480@users.noreply.github.com>
2024-10-02 22:33:28 +0000

    Todo perfe falta una funcion

--- ej02.php
+++ ej02.php
@@ -26,34 +26,29 @@
 
         $cont = fgets(STDIN);
 
+        echo "\n";
+
         switch ($cont) {
             case '1':
                 agregarProducto($listaProductos);
                 break;
             case '2':
-                
+                eliminarProductos($listaProductos);
                 break;
             case '3':
-
+                cambiarProductos($listaProductos);
                 break;
             case '4':
-
                 listarProdcutos($listaProductos);
-                
                 break;
             case '5':
                 
                 break;
             case '6':
-                
                 valortotal($listaProductos);
-
                 break;
-
             case '7':
-                
                 echo "Saliendo del pograma hasta la proxima!!!";
-
                 break;
             default:
                 echo "Error no has seleccionado una opcion";
@@ -82,6 +77,11 @@
 
         $categoria = fgets(STDIN);
 
+        if($precio < 0 || $cantidad < 0){
+
+            echo "\nPrecio y/o cantidad incorrecto";
+            return;
+        }
 
         $productos[] = ['Nombre' => $nombre , 'Precio' => $precio , 'Cantidad' => $cantidad , 'Categoria' => $categoria];
 
@@ -94,13 +94,79 @@
         }else {
             foreach($productos as $variable => $valor ){
 
+                echo "******** PRODUCTO ********\n";
+                echo "ID producto: " . ($variable + 1). "\n";
                 echo "Nombre: {$valor['Nombre']}";
                 echo "Precio:  {$valor['Precio']}";
                 echo "Cantidad: {$valor['Cantidad']}";
                 echo "Categoria: {$valor['Categoria']}";
+                echo "\n-----------------------\n";
             }
         }
 
+    }
+
+    function eliminarProductos(&$productos){
+
+        echo "Introduzca el numero del producto a eliminar: ";
+
+        $id = fgets(STDIN);
+
+        if(empty($productos)){
+
+            echo "La lista de productos esta vacia";
+
+        }else{
+            unset($productos[$id -1]);
+            echo "Producto eliminado perfectamente \n";
+
+            $productos = array_values($productos);
+        }
+
+    }
+
+    function cambiarProductos(&$productos){
+
+        echo "Introduzca el numero del producto a modificar: ";
+
+        $id = fgets(STDIN);
+
+        $id = $id - 1;
+
+        foreach($productos as $variable => $valor ){
+
+            if($id == $variable){
+
+                echo "Introduzca el nuevo nombre del producto: ";
+
+                $nombre = fgets(STDIN);
+
+                echo "Introduzca el nuevo precio del producto: ";
+
+                $precio = fgets(STDIN);
+
+                echo "Introduzca la nueva cantidad del producto: ";
+
+                $cantidad = fgets(STDIN);
+
+                echo "Introduzca la nueva categoria del producto: ";
+
+                $categoria = fgets(STDIN);
+
+                if($precio < 0 || $cantidad < 0){
+
+                    echo "\nPrecio y/o cantidad incorrecto";
+                    break;
+                }
+
+                $productos[$id]['Nombre'] = $nombre;
+                $productos[$id]['Precio'] = $precio;
+                $productos[$id]['Cantidad'] = $cantidad;
+                $productos[$id]['Categoria'] = $categoria;
+
+                echo "\nProducto modificado correctamente\n";
+            }
+        }    
     }
 
     function valortotal($listaProductos){
