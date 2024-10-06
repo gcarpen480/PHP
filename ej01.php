1e8bdf991b0592b7b9c5817cd3e30ab04a4d8a16
gcarpen480 <158757674+gcarpen480@users.noreply.github.com>
2024-10-06 22:48:39 +0000

    Subiendo comentarios

--- ej01.php
+++ ej01.php
@@ -1,16 +1,20 @@
 <?php
 
-    $cont = 0;
+    // Variables que necesitamos para el programa
 
+    // Variables donde almacena el nombre , descripcion y estado de las tareas por defecto la descripocion ya se encuentra en pendiente
     $nombre = "";
     $descripcion = "Pendiente";
     $estado = "";
 
+    // Array donde almacenamos todas las tareas
     $listaTareas = [/*'Titulo' => $nombre , "Descripcion" => $descripcion , "Estado" => $estado*/];
 
     
 
     do {
+
+        // Hacemos un bucle do - while el cual preguntaremos siempre que opcion quiere seleccionar expecto si selecciona la opcion 5 que nos salimos del bucle
 
         echo "
             ------------------------------
@@ -21,36 +25,38 @@
             |  4 - Editar tarea          |
             |  5 - Salir                 |
             |----------------------------|
-        \n";
+        \n"; // Mostramos el menu
 
-        $cont = fgets(STDIN);
+        $cont = fgets(STDIN); // Le pedimos al usuario por teclado un numero para entrar en cada opcion del menu
 
-        echo "\n";
+        echo "\n"; // Salto de linea para machor claridad
 
-        switch ($cont) {
+        switch ($cont) {// Utilizamos un switch el cual tiene como parametro de entrada el valor introducido por teclado
             case '1':
-                añadirTareas($listaTareas);
+                añadirTareas($listaTareas);// Si la opcion es un 1 añadimos un nueva tarea a lista
                 break;
             case '2':
-                listarTareas($listaTareas);
+                listarTareas($listaTareas); // Si la opcion es un 2 listamos las tareas de la lista
                 break;
             case '3':
-                eliminarTarea($listaTareas);
+                eliminarTarea($listaTareas); // Si la opcion es 3 elimanos una tarea de la lista
                 break;
             case '4':
-                cambiarEstado($listaTareas);
+                cambiarEstado($listaTareas); // Si la opcion es 4 cambiados el estado de la tarea
                 break;
             case '5':
-                echo "Saliendo del pograma hasta la proxima!!!";
+                echo "Saliendo del pograma hasta la proxima!!!"; // Si la opcion es 5 salimos del programa
                 break;
             default:
-                echo "Error no has seleccionado una opcion";
+                echo "Error no has seleccionado una opcion"; // Si no pulsamos bien nos dara un error
                 break;
         }
 
-    } while ($cont != 5);
+    } while ($cont != 5); // Cuando pulsemos 5 saldremos el bucle mientras seguiremos dentro
     
-    function añadirTareas(&$tareas){
+    function añadirTareas(&$tareas){ // Creamos una funcion para añadir tareas . Pedimos como parametro una lista
+
+        // Pedimos al usuario que introduzca por teclado tanto el nombre y descripcion de la tarea
 
         echo "Introduzcas el nombre de tu tarea: ";
 
@@ -60,21 +66,26 @@
 
         $descripcion = fgets(STDIN);
 
+        // Añadimos al array quie hemos pasado como parametro un array asociativo el cual contiene 3 claves con sus respectivos valores
 
         $tareas[] = ['Titulo' => $nombre , 'Descripcion' => $descripcion , 'Estado' => 'Pendiente'];
 
     }
 
-    function listarTareas($listaTareas){
+    function listarTareas($listaTareas){// Creamos otra funcion para listar tareas . Pedimos como parametro una lista
         
-        if (empty($listaTareas)) {
+        if (empty($listaTareas)) { // Comprobamos que la lista no se encuentre vacia para listarla
 
-            echo "La lista de tarea esta vacia";
+            echo "La lista de tarea esta vacia"; // Si la lista esta vacia se lo decimos por pantalla
 
         }else {
             
+            // Si la lista no se encuentra vacia la recorremos con un bucle forEach el cual recorremos el array pasado como parametro
+            // El cual tenemos la variable variable la cual coge la clave del array y la variable valor valor la cual contiene los valores de las claves del array
+
             foreach($listaTareas as $variable => $valor ){
 
+                // Vamos imprimiendo el ID de la tarea para poder eliminar la tarea y modificar el estado con mayor facilidad , el titulo , descripcion y el estado
                 echo "******** TAREA ********\n";
                 echo "ID tarea: " . ($variable + 1). "\n";
                 echo "Titulo: " . $valor['Titulo'];
@@ -86,26 +97,31 @@
 
     }
 
-    function eliminarTarea(&$tareas){
+    function eliminarTarea(&$tareas){ // Creamos otra funcion para eliminar las tareas . Pedimos como parametro una lista
 
-        echo "Introduzca el numero de la tarea a eliminar: ";
+        // Pedimos al usuario que introduzca ID de la tarea
+
+        echo "Introduzca el ID de la tarea a eliminar: ";
 
         $indice = fgets(STDIN);
 
-        if(empty($tareas)){
+        if(empty($tareas)){ // Comprobamos que la lista no se encuentre vacia para listarla
 
-            echo "La lista de tarea esta vacia";
+            echo "La lista de tarea esta vacia"; // Si la lista esta vacia se lo decimos por pantalla
 
         }else{
-            unset($tareas[$indice -1]);
-            echo "Tarea eliminada perfectamente \n";
 
-            $tareas = array_values($tareas);
+            unset($tareas[$indice -1]); // Borramos dentro del array la tarea segun el id que haya pasado el usuario
+            echo "Tarea eliminada perfectamente \n"; // Mostramos que la tarea se ha eliminado correctamente
+
+            $tareas = array_values($tareas);// Y reordenamos el array tras elimninar la tarea
         }
 
     }
 
-    function cambiarEstado(&$tareas){
+    function cambiarEstado(&$tareas){ // Creamos otra funcion para cambiar el estado de una tarea . Pedimos como parametro una lista
+
+        // Pedimos al usuario que introduzca ID de la tarea
 
         echo "Introduzca el numero de la tarea a modificar el estado: ";
 
@@ -113,20 +129,24 @@
 
         $id = $id - 1;
 
-        foreach($tareas as $variable => $valor ){
 
-            if($id == $variable){
+
+        foreach($tareas as $variable ){ // Recorremos el array con un bucle ForEach el cual recorre cada elemento del array
+
+            if($id == $variable){// Si el id introducio es igual a al valor variable podemos cambiar el estado
 
                 // echo "Vamos por buen camino";
+
+                // Pedimos al usuario que introduzca el nuevo estado de la tarea
 
                 echo "Introduzca el nuevo estado de la tarea: ";
 
                 $estado = fgets(STDIN);
 
-                $tareas[$id]['Estado'] = $estado;
+                $tareas[$id]['Estado'] = $estado; // Actulizamos el estado dentro del array con el valor introducido por el usuario
 
 
-                echo "Tarea modificada correctamente\n";
+                echo "Tarea modificada correctamente\n"; // Mostramos por pantalla que la modificacion se ha realizado correctamente
                 //break;
 
             }
--- ej02.php
+++ ej02.php
@@ -1,14 +1,19 @@
 <?php
 
-    $cont = 0;
+    // Variables que necesitamos para el programa
+
+    // Variables donde almacena el nombre , precio , cantidad y categoria de los producto
+
     $nombre = "";
     $precio = 0;
     $cantidad = 0;
     $categoria = "";
 
+    // Array donde almacenamos todos los productos
     $listaProductos = [];
 
-
+    // Hacemos un bucle do - while el cual preguntaremos siempre que opcion quiere seleccionar expecto si selecciona la opcion 7 que nos salimos del bucle
+    
     do {
         
         echo "
@@ -22,44 +27,46 @@
             |  6 - Calcular valor total  |
             |  7 - Salir                 |
             |----------------------------|
-        \n";
+        \n";// Mostramos el menu
 
-        $cont = fgets(STDIN);
+        $cont = fgets(STDIN); // Le pedimos al usuario por teclado un numero para entrar en cada opcion del menu
 
-        echo "\n";
+        echo "\n"; // Salto de linea para machor claridad
 
-        switch ($cont) {
+        switch ($cont) {// Utilizamos un switch el cual tiene como parametro de entrada el valor introducido por teclado
             case '1':
-                agregarProducto($listaProductos);
+                agregarProducto($listaProductos);// Si la opcion es un 1 añadimos un nuevo producto a lista
                 break;
             case '2':
-                eliminarProductos($listaProductos);
+                eliminarProductos($listaProductos);// Si la opcion es un 2 eliminamos un producto de la lista
                 break;
             case '3':
-                cambiarProductos($listaProductos);
+                cambiarProductos($listaProductos); // Si la opcion es 3 cambiamos el productos por completo de la lista
                 break;
             case '4':
-                listarProdcutos($listaProductos);
+                listarProdcutos($listaProductos);// Si la opcion es 4 listamos todos los productos de la lista
                 break;
             case '5':
-                filtrasrProductos($listaProductos);
+                filtrasrProductos($listaProductos);// Si la opcion es 5 filtramos los productos segun una categoria
                 break;
             case '6':
-                valortotal($listaProductos);
+                valortotal($listaProductos);// Si la opcion es 6 calculamos el valor total de todos los productos de la lista
                 break;
             case '7':
-                echo "Saliendo del pograma hasta la proxima!!!";
+                echo "Saliendo del pograma hasta la proxima!!!"; // Si la opcion es 7 salimos del programa
                 break;
             default:
-                echo "Error no has seleccionado una opcion";
+                echo "Error no has seleccionado una opcion";// Si no pulsamos bien nos dara un error
                 break;
         }
 
-    } while ($cont != 7);
+    } while ($cont != 7);// Cuando pulsemos 7 saldremos el bucle mientras seguiremos dentro
     
 
 
-    function agregarProducto(&$productos){
+    function agregarProducto(&$productos){ // Creamos una funcion para añadir productos . Pedimos como parametro una lista
+
+        // Pedimos al usuario que introduzca por teclado tanto el nombre , precio , cantidad y categoria de los productos
 
         echo "Introduzcas el nombre del producto: ";
 
@@ -77,23 +84,32 @@
 
         $categoria = fgets(STDIN);
 
+        // Comprobamos si el precio introducido y la cantidad son menores de 0 si son asi le mostramos un mensaje que no pueden ser
+
         if($precio < 0 || $cantidad < 0){
 
             echo "\nPrecio y/o cantidad incorrecto";
             return;
         }
 
+        // Añadimos al array quie hemos pasado como parametro un array asociativo el cual contiene 4 claves con sus respectivos valores
+
         $productos[] = ['Nombre' => $nombre , 'Precio' => $precio , 'Cantidad' => $cantidad , 'Categoria' => $categoria];
 
     }
 
-    function listarProdcutos($productos){
+    function listarProdcutos($productos){// Creamos otra funcion para listar los productos . Pedimos como parametro una lista
 
-        if (empty($productos)) {
-            echo "La lista de productos esta vacia";
+        if (empty($productos)) {// Comprobamos que la lista no se encuentre vacia para listarla
+            echo "La lista de productos esta vacia";// Si la lista esta vacia se lo decimos por pantalla
         }else {
+
+            // Si la lista no se encuentra vacia la recorremos con un bucle forEach el cual recorremos el array pasado como parametro
+            // El cual tenemos la variable variable la cual coge la clave del array y la variable valor valor la cual contiene los valores de las claves del array
+
             foreach($productos as $variable => $valor ){
 
+                // Vamos imprimiendo el ID de los productos para poder eliminar la tarea y modificar el estado con mayor facilidad ademas tambien mostramos el Nombre , Precio , Cantidad y la Categoria
                 echo "******** PRODUCTO ********\n";
                 echo "ID producto: " . ($variable + 1). "\n";
                 echo "Nombre: {$valor['Nombre']}";
@@ -106,36 +122,42 @@
 
     }
 
-    function eliminarProductos(&$productos){
+    function eliminarProductos(&$productos){// Creamos otra funcion para eliminar los productos . Pedimos como parametro una lista
+
+        // Pedimos al usuario que introduzca ID del producto
 
         echo "Introduzca el numero del producto a eliminar: ";
 
         $id = fgets(STDIN);
 
-        if(empty($productos)){
+        if(empty($productos)){ // Comprobamos que la lista no se encuentre vacia para listarla
 
-            echo "La lista de productos esta vacia";
+            echo "La lista de productos esta vacia"; // Si la lista esta vacia se lo decimos por pantalla
 
         }else{
-            unset($productos[$id -1]);
-            echo "Producto eliminado perfectamente \n";
+            unset($productos[$id -1]); // Borramos dentro del array el producto segun el id que haya pasado el usuario
+            echo "Producto eliminado perfectamente \n"; // Mostramos que el producto se ha eliminado correctamente
 
-            $productos = array_values($productos);
+            $productos = array_values($productos);// Y reordenamos el array tras elimninar la tarea
         }
 
     }
 
-    function cambiarProductos(&$productos){
+    function cambiarProductos(&$productos){// Creamos otra funcion para cambiar los productos . Pedimos como parametro una lista
 
-        echo "Introduzca el numero del producto a modificar: ";
+        // Pedimos al usuario que introduzca ID del producto
+
+        echo "Introduzca el numero del producto a modificar: "; 
 
         $id = fgets(STDIN);
 
         $id = $id - 1;
 
-        foreach($productos as $variable => $valor ){
+        foreach($productos as $variable => $valor ){ // Recorremos el array con un bucle ForEach el cual recorre cada elemento del array
 
-            if($id == $variable){
+            if($id == $variable){// Si el id introducio es igual a al valor variable podemos cambiar el producto
+
+                // Pedimos al usuario que introduzca el el nombre , precio , cantidad y categoria de la edicion del producto
 
                 echo "Introduzca el nuevo nombre del producto: ";
 
@@ -153,40 +175,49 @@
 
                 $categoria = fgets(STDIN);
 
+                // Comprobamos si el precio introducido y la cantidad son menores de 0 si son asi le mostramos un mensaje que no pueden ser
+
                 if($precio < 0 || $cantidad < 0){
 
                     echo "\nPrecio y/o cantidad incorrecto";
                     break;
                 }
 
+                // Actualizamos los valores
+
                 $productos[$id]['Nombre'] = $nombre;
                 $productos[$id]['Precio'] = $precio;
                 $productos[$id]['Cantidad'] = $cantidad;
                 $productos[$id]['Categoria'] = $categoria;
 
-                echo "\nProducto modificado correctamente\n";
+                echo "\nProducto modificado correctamente\n"; // Mostramos que los cambios se han realizado correctamente
             }
         }    
     }
 
-    function filtrasrProductos($listaProductos){
+    function filtrasrProductos($listaProductos){// Creamos otra funcion para filtrar los productos . Pedimos como parametro una lista
 
+        // Pedimos al usuario que introduzca una categoria para filtrar
+        
         echo "Introduzca la categoría que desea filtrar: ";
         $categoria = trim(fgets(STDIN));
 
-        $filtrados = [];
+        $filtrados = []; // Creamos un array para colocar los productos filtrados
     
-        foreach ($listaProductos as $producto) {
-            if (trim($producto['Categoria']) === $categoria) {  
-                $filtrados[] = $producto;
+        foreach ($listaProductos as $producto) {// Recorremos el array con un bucle ForEach el cual recorre cada elemento del array
+            if (trim($producto['Categoria']) === $categoria) {  // Comprobamos si la categoria del array coincide con la categoria pasada por el usuario
+                $filtrados[] = $producto;// Si es asi añadimos al array de filtrados el producto para despues mostrarlo por pantalla
             }
         }
 
-        if (empty($filtrados)) {
-            echo "No se encontraron productos de esa categoría '$categoria'.\n";
+        if (empty($filtrados)) { // Comprobamos que la lista no se encuentre vacia para listarla
+            echo "No se encontraron productos de esa categoría '$categoria'.\n";// Si la lista esta vacia se lo decimos por pantalla
         } else {
-            // Display the filtered products
+            // Recorremos el array con un bucle ForEach el cual recorre cada elemento del array
             foreach ($filtrados as $variable => $valor) {
+
+                // Mostramos los productos filtradoss
+
                 echo "******** PRODUCTO ********\n";
                 echo "Nombre: {$valor['Nombre']}";
                 echo "Precio: {$valor['Precio']}";
@@ -198,19 +229,19 @@
 
     }
 
-    function valortotal($listaProductos){
+    function valortotal($listaProductos){// Creamos otra funcion para calcular el valor total los productos . Pedimos como parametro una lista
 
-        $total = 0;
+        $total = 0; // Creamos una variable para almacenar el valor total
 
        
-        foreach($listaProductos as $variable){
+        foreach($listaProductos as $variable){// Recorremos el array con un bucle ForEach el cual recorre cada elemento del array
 
-            $total += $variable['Precio'] * $variable['Cantidad'];
+            $total += $variable['Precio'] * $variable['Cantidad']; // Vamos almacenando y sumando en la variable creada antes el calculo del Precio por la Cantidad
             
         }
         
 
-        echo "Valor total del inventaraio es de = " . $total . " €";
+        echo "Valor total del inventaraio es de = " . $total . " €"; // Mostramos el valor total que tenemos en el inventario
 
     }
 
